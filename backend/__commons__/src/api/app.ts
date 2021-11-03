ac80061561242bc0eee27c59b9499fec5f9581b4
Vagner Kuntz <vaagner.kuntz@gmail.com>
2021-11-03 15:31:37 -0300

    adicionando cors ao commons

--- backend/__commons__/src/api/app.ts
+++ backend/__commons__/src/api/app.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express'
 import helmet from 'helmet'
+import cors from 'cors'
 
 export default (router: Router) => {
   const app = express()
   app.use(helmet())
+  app.use(cors())
   app.use(express.json())
   app.use(router)
 
