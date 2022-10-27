248eb630bb864c6a08d25312b9b7d9787b5fe986
Vagner Kuntz <vaagner.kuntz@gmail.com>
2022-10-27 13:38:04 -0300

    chore: cors validator

--- backend/__commons__/src/api/app.ts
+++ backend/__commons__/src/api/app.ts
@@ -3,11 +3,31 @@ import helmet from 'helmet'
 import cors from 'cors'
 import logger from 'morgan'
 
+function getCorsOrigin() {
+  const origin = process.env.CORS_ORIGIN
+
+  if (!origin) {
+    throw new Error('CORS_ORIGIN is a required env var.')
+  }
+
+  if (origin === '*') {
+    return origin
+  }
+
+  return new RegExp(origin)
+}
+
 export default (router: Router) => {
   const app = express()
   app.use(logger('dev'))
   app.use(helmet())
-  app.use(cors())
+
+  const corOptions = {
+    origin: getCorsOrigin(),
+    optionsSuccessStatus: 200
+  }
+  app.use(cors(corOptions))
+
   app.use(express.json())
   app.use(router)
 
