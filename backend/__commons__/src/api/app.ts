08849e267ccfe93824639ef0ecc65da6a0f3557a
Vagner Kuntz <vaagner.kuntz@gmail.com>
2022-10-27 13:05:20 -0300

    chore: add logger to application

--- backend/__commons__/src/api/app.ts
+++ backend/__commons__/src/api/app.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
+import logger from 'morgan'
 
 export default (router: Router) => {
   const app = express()
+  app.use(logger('dev'))
   app.use(helmet())
   app.use(cors())
   app.use(express.json())
